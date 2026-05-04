61c99ad5f8a9899406dee642419b73705b94b8a4
sbarrerar1605 <sbarrerar1605@gmail.com>
2026-05-04 08:00:30 -0500

    Lab 9

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -4,15 +4,18 @@ model = input("Enter aircraft model:\n")
 plane = Aircraft(model)
 
 while True:
-    command = input("Enter command (A for ascent, D for descent, X to exit):\n")
+    command = input("Enter command (A for ascent, D for descent, X to exit):\n").strip()
 
     if command == "X":
         break
 
     parts = command.split()
 
-    action = parts[0]
-    value = int(parts[1])
+    if len(parts) != 2:
+        continue
+
+    action, value = parts
+    value = int(value)
 
     if action == "A":
         plane.ascend(value)
